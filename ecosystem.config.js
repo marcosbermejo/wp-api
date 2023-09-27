799defeedbbfe6147805c042b027266aa31d4698
Marcos Bermejo <marcosbermejom@gmail.com>
2023-09-27 13:10:47 +0200

    added ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name: 'wp-api',
+      script: 'dist/src/server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '100M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 4000
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user: 'ubuntu',
+      host: '3.249.19.249',
+      ref: 'origin/main',
+      repo: 'git@github.com:marcosbermejo/wp-api.git',
+      path: '/var/www/html/wp-api',
+      key: '/home/marcos/ana.pem',
+      ssh_options: ['ForwardAgent=yes'],
+      'pre-deploy-local': `scp -i /home/marcos/ana.pem .env ubuntu@3.249.19.249:/var/www/html/wp-api/source`,
+      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production'
+    },
+    staging: {},
+    dev : {}
+  }
+};
